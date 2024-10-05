8a4afdcc321c8b7d67d9ca6e3c8d3fb03784d483
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-10-05 18:44:27 +0200

    fix: update web hosts

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ["34.227.11.202", "52.90.153.53"]
+env.hosts = ["3.90.205.72", "54.163.216.239"]
 
 
 def do_clean(number=0):
